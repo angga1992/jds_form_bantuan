22cc33eb3dfe16b8b80f98787cb513e03deb2817
angga1992 <anggamulya.ghazy@gmail.com>
2021-06-07 09:45:56 +0700

    gh-pages

---
+++ precache-manifest.188577961dfc1150324f9fd57270a92e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2419beba736b513c0b",
+    "url": "/jds-test/css/app.d9665a3b.css"
+  },
+  {
+    "revision": "ecdfd9153bf39b7ae2ca621552e69496",
+    "url": "/jds-test/index.html"
+  },
+  {
+    "revision": "5a7855457ea647729aef",
+    "url": "/jds-test/js/about.abd24354.js"
+  },
+  {
+    "revision": "5e2419beba736b513c0b",
+    "url": "/jds-test/js/app.630b5c94.js"
+  },
+  {
+    "revision": "eaa3b10a8adb3b8c79dc",
+    "url": "/jds-test/js/chunk-vendors.6cad2dfe.js"
+  },
+  {
+    "revision": "2ab711f5c1439ac9797f0b06a36415d5",
+    "url": "/jds-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jds-test/robots.txt"
+  }
+]);
